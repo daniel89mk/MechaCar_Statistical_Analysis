0153e8421ded5c1aed26550727cae5dd5878bde5
daniel89mk <kimberlykang0922@gmail.com>
2022-04-03 20:03:22 -0700

    Mecha Car Analysis

---
+++ MechaCarChallenge.R
@@ -0,0 +1,4 @@
+library(dplyr)
+Mecha_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_table)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_table))
